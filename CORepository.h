0b956f1a370e81366749624048660b82c3ed4423
Eric Wasylishen <ewasylishen@gmail.com>
2010-09-20 16:24:39 -0600

    Maybe name the store CORepository?

---
+++ CORepository.h
@@ -0,0 +1,45 @@
+#import <Cocoa/Cocoa.h>
+
+@protocol COStoreDelegate
+
+@optional
+- (void) store: (COStoreCoordinator*)store willCommitChangeset: (COChangeset*)cs;
+- (void) store: (COStoreCoordinator*)store didCommitChangeset: (COChangeset*)cs;
+
+@end
+
+
+@interface CORepository : NSObject
+{
+  COStoreBackend *_backend;
+  id<COStoreDelegate> _delegate;
+}
+
++ (COStore*)storeWithURL: (NSURL*)url;
+
+- (id<COStoreDelegate>)delegate;
+- (void)setDelegate: (id<COStoreDelegate>)aDelegate;
+
+
+
+- (void)permanentlyDeleteObjectsWithUUIDs: (NSArray*)objects;
+
+- (NSDictionary*) propertyListForObjectWithUUID: (ETUUID*)uuid atHistoryGraphNode: (COHistoryGraphNode *)node;
+
+
+@end
+
+@interface COStoreCoordinator (History)
+
+- (ETUUID*) createChangesetWithParentChangesetIdentifiers:
+                      objectUUIDToHashMappings: 
+                              metadataPropertyList:
+                              date:;
+- (id)metadataForChangesetIdentifier:;
+- (NSArray*)parentChangesetIdentifers:
+- (NSArray*)childChangesetIdentifers:;
+- (NSArray*)changesetsModifyingObjectsInSet:
+              beforeDate:
+              afterDate:;
+
+@end
---
+++ CORepository.m
@@ -0,0 +1,14 @@
+//
+//  CORepository.m
+//  ObjectMerging
+//
+//  Created by Eric Wasylishen on 9/20/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "CORepository.h"
+
+
+@implementation CORepository
+
+@end
