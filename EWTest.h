5b6ce7757682a36a7dbadade5f0c558a42fc5fa2
Eric Wasylishen <ewasylishen@gmail.com>
2011-09-25 12:16:05 -0600

    Add mini test framework

---
+++ EWTest.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+
+// Private
+
+void EWTestEqualFunction(id expected, id actual, const char *filename, int line);
+void EWTestIntsEqualFunction(int expected, int actual, const char *filename, int line);
+void EWTestTrueFunction(BOOL flag, const char *filename, int line);
+
+// Public
+
+void EWTestLog();
+
+#define EWTestEqual(expected, actual) EWTestEqualFunction(expected, actual, __FILE__, __LINE__)
+#define EWTestIntsEqual(expected, actual) EWTestIntsEqualFunction(expected, actual, __FILE__, __LINE__)
+#define EWTestTrue(flag) EWTestTrueFunction(flag, __FILE__, __LINE__)
---
+++ EWTest.m
@@ -0,0 +1,42 @@
+#include "EWTest.h"
+
+static unsigned int passes, fails;
+
+void EWTestEqualFunction(id expected, id actual, const char *filename, int line)
+{
+    if ((expected == nil && actual != nil) ||
+        (expected != nil && ![expected isEqual: actual]))        
+    {
+        fails++;
+        NSLog(@"%s:%d: EWTestEqual expected %@, got %@", filename, line, expected, actual);
+    }
+    else
+        passes++;
+}
+
+void EWTestIntsEqualFunction(int expected, int actual, const char *filename, int line)
+{
+    if (expected != actual)   
+    {
+        fails++;
+        NSLog(@"%s:%d: EWTestIntsEqual expected %d, got %d", filename, line, expected, actual);
+    }
+    else
+        passes++;
+}
+
+void EWTestTrueFunction(BOOL flag, const char *filename, int line)
+{
+    if (!flag)   
+    {
+        fails++;
+        NSLog(@"%s:%d: EWTestTrue expected YES got NO", filename, line);
+    }
+    else
+        passes++;
+}
+
+void EWTestLog()
+{
+    NSLog(@"%u/%u passed", passes, passes + fails);
+}
