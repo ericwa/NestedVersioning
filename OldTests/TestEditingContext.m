c3090ca27e19ea8d4ceee049fceeb6cdc596f60b
Eric Wasylishen <ewasylishen@gmail.com>
2011-09-25 21:13:25 -0600

    Add some old test cases for documentation (not hooked up to build)

---
+++ OldTests/TestEditingContext.m
@@ -0,0 +1,258 @@
+#import <Foundation/Foundation.h>
+#import <UnitKit/UnitKit.h>
+#import "COEditingContext.h"
+#import "COContainer.h"
+#import "COCollection.h"
+#import "COStore.h"
+#import "TestCommon.h"
+
+@interface TestEditingContext : NSObject <UKTest>
+{
+}
+@end
+
+@implementation TestEditingContext
+
+- (id) init
+{
+	self = [super init];
+	return self;
+}
+- (void)testCreate
+{
+	COEditingContext *ctx = NewContext();
+	UKNotNil(ctx);
+	TearDownContext(ctx);
+}
+
+- (void)testInsertObject
+{
+	COEditingContext *ctx = NewContext();
+	UKFalse([ctx hasChanges]);
+	
+	
+	COObject *obj = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	UKNotNil(obj);
+	UKTrue([obj isKindOfClass: [COObject class]]);
+	
+	NSArray *expectedProperties = [NSArray arrayWithObjects: @"parentContainer", @"parentCollections", @"contents", @"label", nil];
+	UKObjectsEqual([NSSet setWithArray: expectedProperties],
+				   [NSSet setWithArray: [obj persistentPropertyNames]]);
+
+	UKObjectsSame(obj, [ctx objectWithUUID: [obj UUID]]);
+	
+	UKTrue([ctx hasChanges]);
+	
+	UKNotNil([obj valueForProperty: @"parentCollections"]);
+	UKNotNil([obj valueForProperty: @"contents"]);
+	
+	TearDownContext(ctx);
+}
+
+- (void)testBasicPersistence
+{
+	ETUUID *objUUID;
+	
+	{
+		COStore *store = [[COStore alloc] initWithURL: STORE_URL];
+		COEditingContext *ctx = [[COEditingContext alloc] initWithStore: store];
+		COObject *obj = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+		objUUID = [[obj UUID] retain];
+		[obj setValue: @"Hello" forProperty: @"label"];
+		[ctx commit];
+		[ctx release];
+		[store release];
+	}
+	
+	{
+		COStore *store = [[COStore alloc] initWithURL: STORE_URL];
+		COEditingContext *ctx = [[COEditingContext alloc] initWithStore: store];
+		COObject *obj = [ctx objectWithUUID: objUUID];
+		UKNotNil(obj);
+		NSArray *expectedProperties = [NSArray arrayWithObjects: @"parentContainer", @"parentCollections", @"contents", @"label", nil];
+		UKObjectsEqual([NSSet setWithArray: expectedProperties],
+					   [NSSet setWithArray: [obj persistentPropertyNames]]);
+		UKStringsEqual(@"Hello", [obj valueForProperty: @"label"]);
+		[ctx release];
+		[store release];
+	}
+	[objUUID release];
+	DELETE_STORE;
+}
+
+
+- (void)testDiscardChanges
+{
+	COEditingContext *ctx = NewContext();
+
+	UKFalse([ctx hasChanges]);
+		
+	COObject *o1 = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	ETUUID *u1 = [[o1 UUID] retain];
+	
+	// FIXME: It's not entirely clear what this should do
+	[ctx discardAllChanges];
+	UKNil([ctx objectWithUUID: u1]);
+	
+	UKFalse([ctx hasChanges]);
+	COObject *o2 = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	[o2 setValue: @"hello" forProperty: @"label"];
+	[ctx commit];
+	UKObjectsEqual(@"hello", [o2 valueForProperty: @"label"]);
+	
+	[o2 setValue: @"bye" forProperty: @"label"];
+	[ctx discardAllChanges];
+	UKObjectsEqual(@"hello", [o2 valueForProperty: @"label"]);
+	
+	TearDownContext(ctx);
+}
+
+- (void)testCopyingBetweenContextsWithNoStoreSimple
+{
+	COEditingContext *ctx1 = [[COEditingContext alloc] init];
+	COEditingContext *ctx2 = [[COEditingContext alloc] init];
+
+	COObject *o1 = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	[o1 setValue: @"Shopping" forProperty: @"label"];
+	
+	COObject *o1copy = [ctx2 insertObject: o1];
+	UKNotNil(o1copy);
+	UKObjectsSame(ctx1, [o1 editingContext]);
+	UKObjectsSame(ctx2, [o1copy editingContext]);
+	UKStringsEqual(@"Shopping", [o1copy valueForProperty: @"label"]);
+
+	[ctx1 release];
+	[ctx2 release];
+}
+
+- (void)testCopyingBetweenContextsWithNoStoreAdvanced
+{
+	COEditingContext *ctx1 = [[COEditingContext alloc] init];
+	COEditingContext *ctx2 = [[COEditingContext alloc] init];
+	
+	COContainer *parent = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	COContainer *child = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	COContainer *subchild = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+
+	[parent setValue: @"Shopping" forProperty: @"label"];
+	[child setValue: @"Groceries" forProperty: @"label"];
+	[subchild setValue: @"Pizza" forProperty: @"label"];
+	[child addObject: subchild];
+	[parent addObject: child];
+
+	// We are going to copy 'child' from ctx1 to ctx2. It should copy both
+	// 'child' and 'subchild', but not 'parent'
+	
+	COContainer *childCopy = [ctx2 insertObject: child];
+	UKNotNil(childCopy);
+	UKObjectsSame(ctx2, [childCopy editingContext]);
+	UKNil([childCopy valueForProperty: @"parentContainer"]);
+	UKStringsEqual(@"Groceries", [childCopy valueForProperty: @"label"]);
+	UKNotNil([childCopy contentArray]);
+	
+	COContainer *subchildCopy = [[childCopy contentArray] firstObject];
+	UKNotNil(subchildCopy);
+	UKObjectsSame(ctx2, [subchildCopy editingContext]);
+	UKStringsEqual(@"Pizza", [subchildCopy valueForProperty: @"label"]);
+				   
+	[ctx1 release];
+	[ctx2 release];
+}
+
+- (void)testCopyingBetweenContextsWithSharedStore
+{
+	COEditingContext *ctx1 = NewContext();
+	COEditingContext *ctx2 = [[COEditingContext alloc] initWithStore: [ctx1 store]];
+	
+	COContainer *parent = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	COContainer *child = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	COContainer *subchild = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	
+	[parent setValue: @"Shopping" forProperty: @"label"];
+	[child setValue: @"Groceries" forProperty: @"label"];
+	[subchild setValue: @"Pizza" forProperty: @"label"];
+	[child addObject: subchild];
+	[parent addObject: child];
+	
+	[ctx1 commit];
+	
+	// We won't commit this
+	[parent setValue: @"Todo" forProperty: @"label"];
+	
+	// We'll add another sub-child and leave it uncommitted.
+	COContainer *subchild2 = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	[subchild2 setValue: @"Salad" forProperty: @"label"];
+	[child addObject: subchild2];
+	
+	// We are going to copy 'child' from ctx1 to ctx2. It should copy
+	// 'child', 'subchild', and 'subchild2', but not 'parent' (so 
+	// renaming parent from "Shopping" to "Todo" should not be propagated.)
+	
+	COContainer *childCopy = [ctx2 insertObject: child];
+	UKNotNil(childCopy);
+	UKObjectsSame(ctx2, [childCopy editingContext]);
+	UKObjectsEqual([parent UUID], [[childCopy valueForProperty: @"parentContainer"] UUID]);
+	UKStringsEqual(@"Shopping", [[childCopy valueForProperty: @"parentContainer"] valueForProperty: @"label"]);
+	UKStringsEqual(@"Groceries", [childCopy valueForProperty: @"label"]);
+	UKNotNil([childCopy contentArray]);
+	UKIntsEqual(2, [[childCopy contentArray] count]);
+	if (2 == [[childCopy contentArray] count])
+	{
+		COContainer *subchildCopy = [[childCopy contentArray] firstObject];
+		UKNotNil(subchildCopy);
+		UKObjectsSame(ctx2, [subchildCopy editingContext]);
+		UKStringsEqual(@"Pizza", [subchildCopy valueForProperty: @"label"]);
+		
+		COContainer *subchild2Copy = [[childCopy contentArray] objectAtIndex: 1];
+		UKNotNil(subchild2Copy);
+		UKObjectsSame(ctx2, [subchild2Copy editingContext]);
+		UKStringsEqual(@"Salad", [subchild2Copy valueForProperty: @"label"]);
+	}
+	[ctx2 release];
+	TearDownContext(ctx1);
+}
+
+
+- (void)testCopyingBetweenContextsCornerCases
+{
+	COEditingContext *ctx1 = [[COEditingContext alloc] init];
+	COEditingContext *ctx2 = [[COEditingContext alloc] init];
+	
+	COObject *o1 = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	[o1 setValue: @"Shopping" forProperty: @"label"];
+	
+	COObject *o1copy = [ctx2 insertObject: o1];
+	
+	// Insert again
+	COObject *o1copy2 = [ctx2 insertObject: o1];
+	UKObjectsSame(o1copy, o1copy2);
+	
+	//FIXME: Should inserting again copy over new changes (if any)?
+	
+	[ctx1 release];
+	[ctx2 release];
+}
+
+- (void)testCopyingBetweenContextsWithManyToMany
+{
+	COEditingContext *ctx1 = [[COEditingContext alloc] init];
+	COEditingContext *ctx2 = [[COEditingContext alloc] init];
+
+	COCollection *tag1 = [ctx1 insertObjectWithEntityName: @"Anonymous.Tag"];
+	COContainer *child = [ctx1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+
+	[tag1 addObject: child];
+
+	// Copy the tag collection to ctx2. At first it will be empty since child isn't in ctx2 yet
+	
+	COCollection *tag1copy = [ctx2 insertObject: tag1];
+	UKObjectsEqual([NSArray array], [tag1copy contentArray]);
+	
+	COContainer *childcopy = [ctx2 insertObject: child];
+	UKObjectsEqual([NSArray arrayWithObject: childcopy], [tag1copy contentArray]);
+	
+	[ctx1 release];
+	[ctx2 release];
+}
+
+@end
---
+++ OldTests/TestStore.m
@@ -0,0 +1,218 @@
+#import <Foundation/Foundation.h>
+#import <UnitKit/UnitKit.h>
+#import "COStore.h"
+#import "TestCommon.h"
+
+@interface TestStore : NSObject <UKTest> {
+	
+}
+
+@end
+
+
+@implementation TestStore
+
+static COStore *SetUpStore()
+{
+	if([[NSFileManager defaultManager] fileExistsAtPath: [STORE_URL path]])
+	{
+		BOOL removed = [[NSFileManager defaultManager] removeItemAtPath: [STORE_URL path] error: NULL];
+		assert(removed);
+	}
+	
+	return [[COStore alloc] initWithURL: STORE_URL];
+}
+
+static void TearDownStore(COStore *s)
+{
+	assert(s != nil);
+	NSURL *url = [[s URL] retain];
+	[s release];
+	[[NSFileManager defaultManager] removeItemAtPath: [url path] error: NULL];
+}
+
+
+- (void)testCreate
+{
+	COStore *s = SetUpStore();
+	UKNotNil(s);
+	TearDownStore(s);
+}
+
+- (void)testReopenStore
+{
+	ETUUID *o1 = [ETUUID UUID];
+	NSDictionary *sampleMetadata = [NSDictionary dictionaryWithObject:[NSNumber numberWithBool: YES] forKey:@"metadataWorks"];
+	uint64_t revisionNumber = 0;
+	
+	{
+		COStore *s = [[COStore alloc] initWithURL: STORE_URL];
+
+		[s insertRootObjectUUIDs: S(o1)];		
+		[s beginCommitWithMetadata: sampleMetadata rootObjectUUID: o1];
+		[s beginChangesForObjectUUID: o1];
+		[s setValue: @"bob"
+		forProperty: @"name"
+		   ofObject: o1
+		shouldIndex: NO];
+		[s finishChangesForObjectUUID: o1];
+		CORevision *c1 = [s finishCommit];
+		revisionNumber = [c1 revisionNumber];		
+		[s release];
+	}
+	
+	{
+		COStore *s2 = [[COStore alloc] initWithURL: STORE_URL];
+
+		CORevision *c1 = [s2 revisionWithRevisionNumber: revisionNumber];
+		
+		UKNotNil(c1);
+		
+		UKIntsEqual(1, [[c1 changedObjectUUIDs] count]);
+		if ([[c1 changedObjectUUIDs] count] == 1)
+		{
+			UKObjectsEqual(o1, [[c1 changedObjectUUIDs] objectAtIndex: 0]);
+		}
+		
+		UKObjectsEqual(sampleMetadata, [c1 metadata]);
+		UKObjectsEqual([NSDictionary dictionaryWithObject: @"bob" forKey: @"name"],
+					   [c1 valuesAndPropertiesForObjectUUID: o1]);
+		
+		[s2 release];
+	}
+	
+	[[NSFileManager defaultManager] removeItemAtPath: [STORE_URL path] error: NULL];
+}
+
+
+- (void)testFullTextSearch
+{
+	COStore *s = SetUpStore();
+	
+	ETUUID *o1 = [ETUUID UUID];
+
+	[s insertRootObjectUUIDs: S(o1)];	
+	
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s setValue: @"cats" forProperty: @"name" ofObject: o1 shouldIndex: YES];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c1 = [s finishCommit];
+
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s setValue: @"dogs" forProperty: @"name" ofObject: o1 shouldIndex: YES];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c2 = [s finishCommit];
+	
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s setValue: @"horses" forProperty: @"name" ofObject: o1 shouldIndex: YES];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c3 = [s finishCommit];
+	
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s setValue: @"dogpound" forProperty: @"name" ofObject: o1 shouldIndex: YES];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c4 = [s finishCommit];
+	
+	UKNotNil(c1);
+	UKNotNil(c2);
+	UKNotNil(c3);
+	UKNotNil(c4);
+	
+	NSArray *searchResults = [s resultDictionariesForQuery: @"dog*"];
+	UKIntsEqual(2, [searchResults count]);
+	if ([searchResults count] == 2)
+	{
+		NSDictionary *result1 = [searchResults objectAtIndex: 0];
+		NSDictionary *result2 = [searchResults objectAtIndex: 1];
+		if ([c4 revisionNumber] == [[result1 objectForKey: @"revisionNumber"] unsignedLongLongValue])
+		{
+			id temp = result2; result2 = result1; result1 = temp;
+		}
+		UKObjectsEqual([NSNumber numberWithUnsignedLongLong: [c2 revisionNumber]], [result1 objectForKey: @"revisionNumber"]);
+		UKObjectsEqual(o1, [result1 objectForKey: @"objectUUID"]);
+		UKObjectsEqual(@"name", [result1 objectForKey: @"property"]);
+		UKObjectsEqual(@"dogs", [result1 objectForKey: @"value"]);
+		
+
+		UKObjectsEqual([NSNumber numberWithUnsignedLongLong: [c4 revisionNumber]], [result2 objectForKey: @"revisionNumber"]);
+		UKObjectsEqual(o1, [result2 objectForKey: @"objectUUID"]);
+		UKObjectsEqual(@"name", [result2 objectForKey: @"property"]);
+		UKObjectsEqual(@"dogpound", [result2 objectForKey: @"value"]);
+	}
+	TearDownStore(s);
+}
+
+- (void)testCommitWithNoChanges
+{
+	COStore *s = SetUpStore();
+	
+	ETUUID *o1 = [ETUUID UUID];
+
+	[s insertRootObjectUUIDs: S(o1)];	
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c1 = [s finishCommit];
+	UKNotNil(c1);
+	UKTrue([s isRootObjectUUID: o1]);
+	UKObjectsEqual(S(o1), [s rootObjectUUIDs]);
+
+	TearDownStore(s);
+}
+
+- (void)testRootObject
+{
+	COStore *s = SetUpStore();
+	
+	ETUUID *o1 = [ETUUID UUID];
+	ETUUID *o2 = [ETUUID UUID];
+	ETUUID *o3 = [ETUUID UUID];
+	[s insertRootObjectUUIDs: S(o1)];
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o2];
+	[s setValue: @"cats" forProperty: @"name" ofObject: o2 shouldIndex: NO];
+	[s finishChangesForObjectUUID: o2];
+	[s beginChangesForObjectUUID: o3];
+	[s setValue: @"dogs" forProperty: @"name" ofObject: o3 shouldIndex: NO];
+	[s finishChangesForObjectUUID: o3];
+	CORevision *c1 = [s finishCommit];
+	UKNotNil(c1);
+	UKTrue([s isRootObjectUUID: o1]);
+	UKFalse([s isRootObjectUUID: o2]);
+	UKFalse([s isRootObjectUUID: o3]);
+	UKObjectsEqual(S(o1), [s rootObjectUUIDs]);
+	UKObjectsEqual(S(o1, o2, o3), [s UUIDsForRootObjectUUID: o1]);
+	UKObjectsEqual(o1, [s rootObjectUUIDForUUID: o1]);
+	UKObjectsEqual(o1, [s rootObjectUUIDForUUID: o2]);
+	UKObjectsEqual(o1, [s rootObjectUUIDForUUID: o3]);
+
+	TearDownStore(s);
+}
+
+
+- (void)testStoreNil
+{
+	COStore *s = SetUpStore();
+	
+	ETUUID *o1 = [ETUUID UUID];
+
+	[s insertRootObjectUUIDs: S(o1)];	
+	[s beginCommitWithMetadata: nil rootObjectUUID: o1];
+	[s beginChangesForObjectUUID: o1];
+	[s setValue: nil
+	forProperty: @"name"
+	   ofObject: o1
+	shouldIndex: NO];
+	[s finishChangesForObjectUUID: o1];
+	CORevision *c1 = [s finishCommit];
+
+	UKNotNil(c1);
+	
+	TearDownStore(s);
+}
+
+@end
