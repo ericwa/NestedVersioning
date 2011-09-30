2790f543067bae74e93f3e64023db0beb9834e52
Eric Wasylishen <ewasylishen@gmail.com>
2011-09-30 10:44:53 -0600

    starting a new test (sketch, not actual code)

---
+++ OldTests/TestPhotoLibrary.m
@@ -0,0 +1,19 @@
+#if 0
+
+/*
+ */
+
+// library
+//  |--project1
+//  |--group1
+//  |   |-project2
+//  |   |   |-leaf1	
+//  |   |   |
+//  |   |   \-leaf2
+//  |    \-group2
+//  |       \-leaf3
+//  |--master1
+//  |--masert2
+//  \--master3
+
+#endif
