cbc0bd97cf9542bd506d24f30aab3070f91b0229
Eric Wasylishen <ewasylishen@gmail.com>
2011-11-09 17:08:57 -0700

    implement naviagation of persistent root objects

--- COStoreController.h
+++ COStoreController.h
@@ -1,15 +1,75 @@
 #import <Foundation/Foundation.h>
 #import "COStore.h"
+#import "COPath.h"
+
+
 
 /**
  * a store api one level higher than COStore..
  * probably this will become the real COStore api once we switch to sqlite again
- */
+
+
+detailed specification of persistent root plist object:
+
+-note that a persistent root contains its branches.
+-copying a persistent root copies the branches, and should probably
+give the root and branches new uuid's.
+-note that you can refer to the branch directly without the parent root's uuid.
+
+{
+	"type" : "root"
+	"name" : "the object's name"
+	"uuid" : 0d7489b0-0a9d-11e1-be50-0800200c9a66
+	
+	"tracking-type" : "owned-branch" or "remote-root" or "remote-branch" or "version"
+	"tracking" : one of the following:
+		a) the uuid of one of the branches we own,
+		b) or a path to another persistent root,
+		c) or a path to a branch owned by another persistent root,
+		d) or a specific version.
+	
+	"branches" : (
+				  {
+					  "type" : "branch"
+					  "uuid" : "8a099b84-09eb-4a3e-828d-9a897778e5e3"
+					  "owning-root-uuid" : 0d7489b0-0a9d-11e1-be50-0800200c9a66 // the uuid of the enclosing root
+					  "name" : "whatever you want to call it"
+					  "version" : version-uuid 
+				  },
+				  {
+					  "type" : "branch"
+					  "uuid" : "cdf68e39-8f4b-4afa-9f81-ba2f7cdf50e6"
+					  "owning-root-uuid" : 0d7489b0-0a9d-11e1-be50-0800200c9a66 // the uuid of the enclosing root
+					  "name" : "another branch"
+					  "version" : version-uuid 
+				  },
+			)
+	}
+**/
+
 @interface COStoreController : NSObject
 {
 	COStore *store;
 }
 - (id)initWithStore: (COStore*)aStore;
 
+/** @taskunit reading */
+
+/**
+ * this is the core/primitive method for navigating a path.
+ *
+ * it needs to know how to navigate through the embedded objects
+ * which represent persistent roots/branches.
+ *
+ * It returns the uuid of the current version which the path
+ * points to.
+ */
+- (ETUUID*) currentVersionForPersistentRootAtPath: (COPath*)path;
+
+/**
+ * simple wrapper around -[COStore UUIDsAndPlistsForCommit:]
+ */
+- (id) plistForEmbeddedObject: (ETUUID*)embeddedObject
+					 inCommit: (ETUUID*)aCommitUUID;
 
 @end
--- COStoreController.m
+++ COStoreController.m
@@ -10,4 +10,94 @@
 	return self;
 }
 
+/**
+ * see comment in header
+ */
+- (ETUUID*) currentVersionForPersistentRootAtPath: (COPath*)path
+{
+	if ([path isEmpty])
+	{
+		return [store rootVersion];
+	}
+	else
+	{
+		COPath *parent = [path pathByDeletingLastPathComponent];
+		ETUUID *lastPathComponent = [path lastPathComponent];
+		
+		// recursive call to ourself to find the version containing the last
+		// path component.
+		ETUUID *parentCurrentVersion = [self currentVersionForPersistentRootAtPath: parent];
+
+		id embeddedObjectPlist = [self plistForEmbeddedObject: lastPathComponent
+													 inCommit: parentCurrentVersion];
+		
+		
+		NSString *type = [embeddedObjectPlist objectForKey: @"type"];
+		
+		if ([type isEqualToString: @"root"])
+		{
+			NSString *trackingType = [embeddedObjectPlist objectForKey: @"tracking-type"];
+			NSString *tracking = [embeddedObjectPlist objectForKey: @"tracking"];
+			if ([trackingType isEqualToString: @"owned-branch"])
+			{
+				// tracking is a single UUID of the branch.
+				id branchPlist = [self plistForEmbeddedObject: [ETUUID UUIDWithString: tracking]
+													 inCommit: parentCurrentVersion];
+				
+				ETUUID *uuid = [ETUUID UUIDWithString:
+								[branchPlist objectForKey: @"version"]];
+				return uuid;
+			}
+			else if ([trackingType isEqualToString: @"remote-root"] ||
+					 [trackingType isEqualToString: @"remote-branch"])
+			{
+				// tracking is a full path to a branch/root in another persistent root
+				
+				COPath *trackingPath = [COPath pathWithString: tracking];
+				
+				// FIXME: could be an infinite loop if a root is tracking itself.
+				return [self currentVersionForPersistentRootAtPath: trackingPath];
+			}
+			else if ([trackingType isEqualToString: @"version"])
+			{
+				// tracking is a version, so effectively the persistent root is just a branch
+				
+				ETUUID *uuid = [ETUUID UUIDWithString: tracking];
+				return uuid;
+			}
+
+			[NSException raise: NSInternalInconsistencyException
+						format: @"unsupported tracking type %@: %@", trackingType, tracking];
+		}
+		else if ([type isEqualToString: @"branch"])
+		{
+			ETUUID *uuid = [ETUUID UUIDWithString:
+								[embeddedObjectPlist objectForKey: @"version"]];
+			return uuid;
+		}
+		
+		[NSException raise: NSInternalInconsistencyException
+					format: @"failed to parse %@", embeddedObjectPlist];
+		return nil;
+	}
+}
+
+- (id) plistForEmbeddedObject: (ETUUID*)embeddedObject
+					 inCommit: (ETUUID*)aCommitUUID
+{
+	NILARG_EXCEPTION_TEST(embeddedObject);
+	NILARG_EXCEPTION_TEST(aCommitUUID);
+	
+	NSDictionary *dict = [store UUIDsAndPlistsForCommit: aCommitUUID];
+	id plist = [dict objectForKey: embeddedObject];
+	
+	if (plist == nil)
+	{
+		[NSException raise: NSInvalidArgumentException
+					format: @"%@ not found in commit %@", embeddedObject, aCommitUUID];
+	}
+	
+	return plist;
+}
+
 @end
