4fd9f70e6bcee4bac8dbc1cc77ac6c10ae83befc
Eric Wasylishen <ewasylishen@gmail.com>
2011-11-17 16:41:10 -0700

    sketch out COStoreItem implementation

--- COStoreItem.h
+++ COStoreItem.h
@@ -1,6 +1,53 @@
 #import <Foundation/Foundation.h>
 #import "ETUUID.h"
 
+NSString * const kCOTypeKind;
+NSString * const kCOPrimitiveTypeKind;
+NSString * const kCOContainerTypeKind;
+
+NSString * const kCOPrimitiveType;
+NSString * const kCOPrimitiveTypeInt64;
+NSString * const kCOPrimitiveTypeDouble;
+NSString * const kCOPrimitiveTypeString;
+NSString * const kCOPrimitiveTypeBlob;
+NSString * const kCOPrimitiveTypeCommitUUID; // just a version uuid. prevents version from being GC'ed.
+NSString * const kCOPrimitiveTypeHoldingPath; // prevents destination from being GC'ed.        copied when parent is copied
+NSString * const kCOPrimitiveTypeReferencePath;// doesn't prevent destination from being GC'ed. not copied when parent is copied
+
+NSString * const kCOContainerOrdered;
+NSString * const kCOContainerAllowsDuplicates;
+
+/**
+examples of type dictionaries:
+ 
+ { kCOTypeKind : kCOPrimitiveTypeKind;
+   kCOPrimitiveType : kCOPrimitiveTypeInt64 }
+ 
+ { kCOTypeKind : kCOContainerTypeKind;
+   kCOPrimitiveType : kCOPrimitiveTypeHoldingPath;
+   kCOContainerOrdered : YES;
+   kCOContainerAllowsDuplicates : NO } 
+ 
+ObjC values for types:
+ 
+kCOPrimitiveTypeInt64: NSNumber containing longLong
+kCOPrimitiveTypeDouble: NSNumber containing double
+kCOPrimitiveTypeString: NSString
+kCOPrimitiveTypeBlob: NSData
+kCOPrimitiveTypeCommitUUID: ETUUID
+kCOPrimitiveTypeHoldingPath: COPath
+kCOPrimitiveTypeReferencePath: COPath
+
+for containers:
+ 
+kCOContainerOrdered = NO, kCOContainerAllowsDuplicates = NO: NSSet
+kCOContainerOrdered = NO, kCOContainerAllowsDuplicates = YES: NSCountedSet 
+kCOContainerOrdered = YES, kCOContainerAllowsDuplicates = NO: NSArray
+kCOContainerOrdered = YES, kCOContainerAllowsDuplicates = YES: NSArray 
+ 
+*/
+
+
 /**
  */
 @interface COStoreItem : NSObject
@@ -15,64 +62,11 @@
 
 - (NSArray *) attributeNames;
 
-/**
+- (NSDictionary *) typeForAttribute: (NSString *)anAttribute;
+- (id) valueForAttribute: (NSString*)anAttribute;
 
-scalar types: 
-
-in64_t, double, string, blob,
-version,       // just a version uuid. prevents version from being GC'ed.
-holding-path,  // prevents destination from being GC'ed.        copied when parent is copied
-reference-path // doesn't prevent destination from being GC'ed. not copied when parent is copied
-
-collecion types:
- 
-({scalar_type}, ordered={no/yes}, allows_duplicates={no/yes})
- 
-
-*/
-
-
-
-
-
-// old:
-
-/*
-// scalar types
-
-- (void) setIntegerValue: (int64_t)anInt
-		forAttributeName: (NSString*)attributeName
-				typeName: (NSString*)typeName;
-
-- (void) setDoubleValue: (double)aReal
-		forAttributeName: (NSString*)attributeName
-				typeName: (NSString*)typeName;
-
-- (void) setStringValue: (NSString*)aStr
-	   forAttributeName: (NSString*)attributeName
-			   typeName: (NSString*)typeName;
-
-- (void) setBlobValue: (NSData*)aData
-	forAttributeName: (NSString*)attributeName
-			typeName: (NSString*)typeName;
-
-// multivalued types
-
-- (void) setArrayOfIntegers: (NSArray *)anArray
-		forAttributeName: (NSString*)attributeName
-				typeName: (NSString*)typeName;
-
-- (void) setArrayOfDoubles: (NSArray *)anArray
-	   forAttributeName: (NSString*)attributeName
-			   typeName: (NSString*)typeName;
-
-- (void) setArrayOfStrings: (NSArray *)anArray
-		  forAttributeName: (NSString*)attributeName
-				  typeName: (NSString*)typeName;
-
-- (void) setArrayOfBlobs: (NSArray *)anArray
-		 forAttributeName: (NSString*)attributeName
-				 typeName: (NSString*)typeName;
-*/
+- (void) setValue: (id)aValue
+	 forAttribute: (NSString*)anAttribute
+			 type: (NSDictionary*)aType;
 
 @end
--- COStoreItem.m
+++ COStoreItem.m
@@ -1,5 +1,53 @@
 #import "COStoreItem.h"
 
+NSString * const kCOTypeKind = @"kCOTypeKind";
+NSString * const kCOPrimitiveTypeKind = @"kCOPrimitiveTypeKind";
+NSString * const kCOContainerTypeKind = @"kCOContainerTypeKind";
+
+NSString * const kCOPrimitiveType = @"kCOPrimitiveType";
+NSString * const kCOPrimitiveTypeInt64 = @"kCOPrimitiveTypeInt64";
+NSString * const kCOPrimitiveTypeDouble = @"kCOPrimitiveTypeDouble";
+NSString * const kCOPrimitiveTypeString = @"kCOPrimitiveTypeString";
+NSString * const kCOPrimitiveTypeBlob = @"kCOPrimitiveTypeBlob";
+NSString * const kCOPrimitiveTypeCommitUUID = @"kCOPrimitiveTypeCommitUUID";
+NSString * const kCOPrimitiveTypeHoldingPath = @"kCOPrimitiveTypeHoldingPath";
+NSString * const kCOPrimitiveTypeReferencePath = @"kCOPrimitiveTypeReferencePath";
+
+NSString * const kCOContainerOrdered = @"kCOContainerOrdered";
+NSString * const kCOContainerAllowsDuplicates = @"kCOContainerAllowsDuplicates";
+
 @implementation COStoreItem
+
+- (ETUUID *)uuid
+{
+	return uuid;
+}
+
+- (NSArray *) attributeNames
+{
+	assert([[types allKeys] isEqual: [values allKeys]]);	
+	return [types allKeys];
+}
+
+- (NSDictionary *) typeForAttribute: (NSString *)anAttribute
+{
+	return [types objectForKey: anAttribute];
+}
+
+- (id) valueForAttribute: (NSString*)anAttribute
+{
+	return [values objectForKey: anAttribute];
+}
+
+
+- (void) setValue: (id)aValue
+	 forAttribute: (NSString*)anAttribute
+			 type: (NSDictionary*)aType
+{
+	// FIXME: validate
+	[types setObject: aType forKey: anAttribute];
+	[values setObject: aValue forKey: anAttribute];
+}
+
 
 @end
