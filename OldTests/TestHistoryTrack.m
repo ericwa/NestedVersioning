951fe0796faa903f4edf24b3bd82e6e1df287198
Eric Wasylishen <ewasylishen@gmail.com>
2011-11-21 15:47:40 -0700

    start to rework TestHistoryTrack

--- OldTests/TestHistoryTrack.m
+++ OldTests/TestHistoryTrack.m
@@ -92,7 +92,11 @@
 	//   \-document2 <<persistent root>>
 	
 	
-	[document2 addObject: group2]; [ctx commit];
+	/**
+	 * this is a move of embedded obejct tree
+	 * implemented as copy + delete source
+	 */
+	[document2 addObject: group2]; [ctx commit]; // doc 1 and doc 2
 	
 	// workspace <<persistent root>>
 	//  |
@@ -111,13 +115,13 @@
 	//          \-leaf2
 	
 	
-	[group2	setValue: @"Groceries" forProperty: @"label"]; [ctx commit];
-	[group1 setValue: @"Work Contacts" forProperty: @"label"]; [ctx commit];
-	[leaf3 setValue: @"Wine" forProperty: @"label"]; [ctx commit];
-	[leaf1 setValue: @"Alice" forProperty: @"label"]; [ctx commit];
-	[leaf3 setValue: @"Red wine" forProperty: @"label"]; [ctx commit];
-	[leaf2 setValue: @"Cheese" forProperty: @"label"]; [ctx commit];
-	[leaf1 setValue: @"Alice (cell)" forProperty: @"label"]; [ctx commit];
+	[group2	setValue: @"Groceries" forProperty: @"label"]; [ctx commit]; // doc2
+	[group1 setValue: @"Work Contacts" forProperty: @"label"]; [ctx commit]; // doc1
+	[leaf3 setValue: @"Wine" forProperty: @"label"]; [ctx commit]; // doc2
+	[leaf1 setValue: @"Alice" forProperty: @"label"]; [ctx commit]; // doc1
+	[leaf3 setValue: @"Red wine" forProperty: @"label"]; [ctx commit]; // doc2
+	[leaf2 setValue: @"Cheese" forProperty: @"label"]; [ctx commit]; // doc2
+	[leaf1 setValue: @"Alice (cell)" forProperty: @"label"]; [ctx commit]; // doc1
 	
 	// introduce some new objects
 	
@@ -131,7 +135,7 @@
 	
 	// add them to the lists
 
-	[group1 addObject: leaf4]; [ctx commit];
+	[group1 addObject: leaf4]; [ctx commit]; // doc1
 	
 	// workspace <<persistent root>>
 	//  |
@@ -153,7 +157,7 @@
 	
 	
 	
-	[group2 addObject: leaf5]; [ctx commit];
+	[group2 addObject: leaf5]; [ctx commit]; // doc2
 
 	// workspace <<persistent root>>
 	//  |
@@ -177,7 +181,7 @@
 	
 	
 	
-	[group2 addObject: leaf6]; [ctx commit];
+	[group2 addObject: leaf6]; [ctx commit]; // doc2
 
 	// workspace <<persistent root>>
 	//  |
@@ -202,9 +206,9 @@
 	//          \-leaf6
 	
 	
-	[leaf4 setValue: @"Carol" forProperty: @"label"]; [ctx commit];
-	[leaf5 setValue: @"Pizza" forProperty: @"label"]; [ctx commit];
-	[leaf6 setValue: @"Beer" forProperty: @"label"]; [ctx commit];	
+	[leaf4 setValue: @"Carol" forProperty: @"label"]; [ctx commit]; // doc1
+	[leaf5 setValue: @"Pizza" forProperty: @"label"]; [ctx commit]; // doc2
+	[leaf6 setValue: @"Beer" forProperty: @"label"]; [ctx commit];	// doc2
 	
 	UKFalse([ctx hasChanges]);
 	
@@ -224,103 +228,27 @@
 	
 	// Start with an easy test
 	
-	
+	// NB. leaf3 was always in doc1
 	UKObjectsEqual(@"Red wine", [leaf3 valueForProperty: @"label"]);
-	[leaf3Track undo];
+	[leaf3Track undo]; // selective undo in doc1 -> creates a new commit
 	UKObjectsEqual(@"Wine", [leaf3 valueForProperty: @"label"]);
+	[leaf3Track undo]; // selective undo in doc1 -> creates a new commit
+	UKObjectsEqual(@"Leaf 3", [leaf3 valueForProperty: @"label"]);
+	
+	
+	// Now try undoing changes made to Document 1, using doc1track. 
+	// "It shouldn't affect leaf3 until seveal steps back in to the history." (2010)
+	// wrong, actually, the first thing it will do is undo the selective undos made
+	// on leaf3 (nov 2011)
+	
+	// -undo label:wine -> label:leaf3 on leaf3
+	// -undo label:red wine -> label:wine on leaf3
+	// -undo label:carol -> label:leaf 4 on leaf4
+	// -undo add leaf4 to group1
+	// -undo label:alice -> label:alice (cell) on leaf1
+    // -undo label:group  1 -> label:work contacts on group1
+	// -undo the deletion part of move group2 from doc1 to doc2
 
-	// FIXME: [leaf3Track undo];
-	//UKObjectsEqual(@"Leaf 3", [leaf3 valueForProperty: @"label"]);
-	//UKObjectsEqual(S([leaf3 UUID]), [ctx changedObjectUUIDs]); // Ensure that no other objects were changed by the history track
-	
-	
-	// Now try undoing changes made to Document 1, using doc1track. It shouldn't 
-	// affect leaf3 until seveal steps back in to the history.
-	
-	
-	// first undo should change leaf4's label from "Carol" -> "Leaf 4"
-	UKObjectsEqual(@"Carol", [leaf4 valueForProperty: @"label"]);
-	[doc1Track undo]; 
-	UKObjectsEqual(@"Leaf 4", [leaf4 valueForProperty: @"label"]);
-	//UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID]), [ctx changedObjectUUIDs]);
-	
-	// next undo should remove leaf4 from group1
-	UKObjectsEqual(S([leaf1 UUID], [leaf4 UUID]), [[[NSSet setWithArray: [group1 contentArray]] mappedCollection] UUID]);
-	//[doc1Track undo]; 
-	//UKObjectsEqual(S([leaf1 UUID]), [[[NSSet setWithArray: [group1 contentArray]] mappedCollection] UUID]);	
-	//UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID]), [ctx changedObjectUUIDs]);
-
-	// FIXME: Undo doesn't work in the code below.
-
-/*
-	// next undo should change leaf1's label from "Alice (cell)" -> "Alice"
-	UKObjectsEqual(@"Alice (cell)", [leaf1 valueForProperty: @"label"]);
-	[doc1Track undo]; 
-	UKObjectsEqual(@"Alice", [leaf1 valueForProperty: @"label"]);
-	//UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID]), [ctx changedObjectUUIDs]);
-	
-	// next undo should change group1's label from "Work Contacts" -> "Group 1"
-	UKObjectsEqual(@"Work Contacts", [group1 valueForProperty: @"label"]);
-	[doc1Track undo]; 
-	UKObjectsEqual(@"Group 1", [group1 valueForProperty: @"label"]);
-	//UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID]), [ctx changedObjectUUIDs]);
-	
-	// next undo should move group2 from document 2 back to document 1
-	UKTrue([[document2 contentArray] containsObject: group2]);
-	UKFalse([[document1 contentArray] containsObject: group2]);
-	UKObjectsSame(document2, [group2 valueForProperty: @"parentContainer"]);
-	[doc1Track undo]; 
-	UKFalse([[document2 contentArray] containsObject: group2]);
-	UKTrue([[document1 contentArray] containsObject: group2]);
-	UKObjectsSame(document1, [group2 valueForProperty: @"parentContainer"]);
-	//UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID], [document2 UUID], [group2 UUID], [document1 UUID]), [ctx changedObjectUUIDs]);
-*/	
-	// FIXME: After group 2 is moved back to doc1, the next undo will actually
-	// be the newest changes in group 2 (e.g. Leaf 6 -> Beer, and Leaf 5 -> Pizza)
-	// So the following tests need to be rewritten.
-	
-/*	
-	// next undo should move leaf2 ("Tomatoes") from group 2 to group 1
-	UKTrue([[group2 contentArray] containsObject: leaf2]);
-	UKFalse([[group1 contentArray] containsObject: leaf2]);
-	UKObjectsSame(group2, [leaf2 valueForProperty: @"parentContainer"]);
-	[doc1Track undo]; 
-	UKFalse([[group2 contentArray] containsObject: leaf2]);
-	UKTrue([[group1 contentArray] containsObject: leaf2]);
-	UKObjectsSame(group1, [leaf2 valueForProperty: @"parentContainer"]);
-	UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID], [document2 UUID], [group2 UUID], [document1 UUID], [leaf2 UUID]), [ctx changedObjectUUIDs]);
-	
-	// next undo should rename leaf2 "Tomatoes" -> "Leaf 2"
-	UKObjectsEqual(@"Tomatoes", [leaf2 valueForProperty: @"label"]);
-	[doc1Track undo]; 
-	UKObjectsEqual(@"Leaf 2", [leaf2 valueForProperty: @"label"]);
-	UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID], [document2 UUID], [group2 UUID], [document1 UUID], [leaf2 UUID]), [ctx changedObjectUUIDs]);
-
-	// next undo should rename document 1 "My Contacts" -> "Document 1"
-	UKObjectsEqual(@"My Contacts", [document1 valueForProperty: @"label"]);
-	[doc1Track undo]; 
-	UKObjectsEqual(@"Document 1", [document1 valueForProperty: @"label"]);
-	UKObjectsEqual(S([leaf3 UUID], [leaf4 UUID], [group1 UUID], [leaf1 UUID], [document2 UUID], [group2 UUID], [document1 UUID], [leaf2 UUID]), [ctx changedObjectUUIDs]);
-	
-	// FIXME: The next undo would undo the initial commit of Document 1.
-	// Need to decide what happens if you try to undo it
-	
-	
-	
-	//
-	// Now we will test a more complicated scenario: performing undo/redo on
-	// document 2.
-	//
-	
-	
-	UKObjectsEqual(@"Groceries", [group2 valueForProperty: @"label"]); // Verify that the state of document2 wasn't changed (other than moving group2 back to document 1)
-	[doc2Track undo];
-	// FIXME
-*/
-	
-	[workspaceTrack release];
-	[doc1Track release];
-	[doc2Track release];
-	TearDownContext(ctx);
+	// FIXME: continue
 }
 @end
