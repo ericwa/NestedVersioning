79b03f5cc620d6074b531767a48bba79b058c2d8
Eric Wasylishen <ewasylishen@gmail.com>
2011-11-22 16:08:20 -0700

    expand game editor test a bit

--- OldTests/TestUndo.m
+++ OldTests/TestUndo.m
@@ -8,17 +8,5 @@
  
  this sounds undesirable at first glance - motivates storing composition
  relationships in the parent as a list of children.
- 
- 
- ==== 
 
- just pasting this here:
- 
- I’m still not sure how to model a QuArK (3D game level editor) project-like document with various revision control possibilities: revision control graph per map, for the entire project, and undo over all changes. What about undo in the editor vs undo in the project? What about two editor windows open on the same map, and undo for each? What about undo of editor (view) data (scrollbar position, selection, etc, that shouldn’t be stored directly in the model? ) What about branching the project vs branching a map? Linking and embedding cross map and cross project? That about covers all the problems I can think of right now...
-
- 
- 
- 
- 
- 
 */
