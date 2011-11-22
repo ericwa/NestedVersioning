55deb4c822b6c47bbc5335d21379834fd6df22e9
Eric Wasylishen <ewasylishen@gmail.com>
2011-11-22 15:48:38 -0700

    add a tagging test

---
+++ OldTests/TestTagging.m
@@ -0,0 +1,49 @@
+#if 0
+
+void test()
+{	
+	// tag library <<persistent root>>
+	//  |
+	//  \--places
+	//      |
+	//      |-north america
+	//      |   |
+	//      |   \-canada 
+	//      |
+	//      \-south america
+	//          |
+	//          \-brazil
+	//    
+	// photo library <<persistent root>>
+	//  |
+	//  |--local tags
+	//  |   |
+	//  |   |-subject
+	//  |   |   |
+	//  |   |   |-landscape 
+	//  |   |   |
+	//  |   |   |-people
+	//  |   |   |
+	//  |   |   \-abstract
+	//  |   |
+	//  |   \-lighting
+	//  |       |
+	//  |       |-sunlight
+	//  |       |
+	//  |       \-artificial
+	//  | 
+	//   \-photo shoots
+	//      |
+	//      \--shoot1
+    //          |
+	//          |--photo1 (tags: places/north america/canada, subject/landscape, subject/abstract)
+    //          |
+	//          |--photo2 (tags: lighting/sunlight, places/south america/brazil, subject/abstract)
+    //          |
+	//          \--photo3 (tags: lighting/artificial, places/south america/brazil, subject/people)
+
+	
+	
+}
+
+#endif
