969a8bc695e1af5e2a2217f01da7d2479c3f237c
Eric Wasylishen <ewasylishen@gmail.com>
2011-12-05 00:29:26 -0700

    add storebrowser app subproject

---
+++ StoreBrowser/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoreBrowser
+//
+//  Created by Eric Wasylishen on 11-12-05.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ StoreBrowser/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  StoreBrowser
+//
+//  Created by Eric Wasylishen on 11-12-05.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
