6d40844f7ad6c92d1e8ae6373e4e60aea399b26a
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-06 21:45:23 -0700

    edit documentation a bit

--- COPath.h
+++ COPath.h
@@ -13,20 +13,6 @@
  * 1. A COPersistentRootEditingContext instance has a path so you can create a
  *    context for editing a nested persistent root.
  *
- *    e.g. if an editing context was created for 
- *         the path "<photo library uuid>/<photo 1>", the path is used both
- *         when creating the context, to look up the current version of the
- *         photo library, and then look up the current version of "photo 1"
- *         inside the current photo library.
- *
- *         each UUID the path is the UUID of an item which should have
- *         "type" = "persistentRoot" or "type" = "branch". the first item 
- *         must be in the store's root tree, the second one is in the item tree
- *         inside the commit which the first persistent root refers to, etc.
- * 
- *         the path is also used when making a commit because each persistent
- *         root in the path needs to be updated.
- *
  * 2. As a data type of a value in an item inside a persistent root.
  *    
  *    a) paths with a single element (basically just a UUID) are used for 
