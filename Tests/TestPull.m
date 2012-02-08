1044263a6043142dbd0df2702331d90b6217c596
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-08 01:27:47 -0700

    add a full test of the pull feature, which will require merging array contents to pass.

--- Tests/TestPull.m
+++ Tests/TestPull.m
@@ -1,14 +1,10 @@
 #import "TestCommon.h"
 
-void testPull()
+static void testPullSimpleFastForward(void);
+static void testPullComplex(void);
+
+static void testPullSimpleFastForward()
 {
-	// commits in the persistent root
-	
-	ETUUID *commit1 = nil; // initial commit, "red"
-	ETUUID *commit2 = nil; // "orange"
-	ETUUID *commit3 = nil; // "yellow"
-	
-	
 	// setup a simple persistent root containing { "color" : "red" }
 	
 	COStore *store = setupStore();
@@ -50,8 +46,7 @@ void testPull()
 						forAttribute: @"color"
 								type: [COType stringType]];
 		
-		commit2 = [ctx2 commitWithMetadata: nil];
-		commit1 = [store parentForCommit: commit2];
+		[ctx2 commitWithMetadata: nil];
 	}
 	
 	// make a commit in the persistent root (which is on branch A) { "color" : "yellow" }
@@ -62,7 +57,7 @@ void testPull()
 		[contents3 setPrimitiveValue: @"yellow"
 						forAttribute: @"color"
 								type: [COType stringType]];
-		commit3 = [ctx3 commitWithMetadata: nil];
+		[ctx3 commitWithMetadata: nil];
 	}
 	
 		
@@ -77,7 +72,6 @@ void testPull()
 	
 	// test that we can read the document contents as expected.
 	
-	EWTestEqual(@"yellow", [[[ctx editingContextForEditingEmbdeddedPersistentRoot: i1] persistentRootTree] valueForAttribute: @"color"]);
 	EWTestEqual(@"yellow", [[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchA] persistentRootTree] valueForAttribute: @"color"]);
 	EWTestEqual(@"red", [[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchB] persistentRootTree] valueForAttribute: @"color"]);	
 	
@@ -91,7 +85,189 @@ void testPull()
 	
 	[ctx commitWithMetadata: nil];
 	
-	EWTestEqual(@"yellow", [[[ctx editingContextForEditingEmbdeddedPersistentRoot: i1] persistentRootTree] valueForAttribute: @"color"]);
-	EWTestEqual(@"yellow", [[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchA] persistentRootTree] valueForAttribute: @"color"]);
 	EWTestEqual(@"yellow", [[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchB] persistentRootTree] valueForAttribute: @"color"]);	
+}
+
+
+
+
+
+
+// Helper functions for testPullComplex()
+
+/**
+ * { "letters" : ("b", "c", "d") }
+ */
+static COSubtree *subtreeInitialVersion(ETUUID *aUUID) 
+{
+	COSubtree *tree = [[[COSubtree alloc] initWithUUID: aUUID] autorelease];
+	
+	[tree addObject: @"b"
+ toOrderedAttribute: @"letters"
+			atIndex: 0
+			   type: [COType arrayWithPrimitiveType: [COType stringType]]];
+	
+	[tree addObject: @"c"
+ toOrderedAttribute: @"letters"
+			atIndex: 1
+			   type: [COType arrayWithPrimitiveType: [COType stringType]]];
+	
+	[tree addObject: @"d"
+ toOrderedAttribute: @"letters"
+			atIndex: 2
+			   type: [COType arrayWithPrimitiveType: [COType stringType]]];
+	
+	return tree;
+}
+
+/**
+ * { "letters" : ("a", "b", "c", "d") }
+ */
+static COSubtree *subtreeVariantA(ETUUID *aUUID)
+{
+	COSubtree *tree = subtreeInitialVersion(aUUID);
+		
+	[tree addObject: @"a"
+ toOrderedAttribute: @"letters"
+			atIndex: 0
+			   type: [COType arrayWithPrimitiveType: [COType stringType]]];
+		
+	return tree;
+}
+
+/**
+ * { "letters" : ("b", "c", "d", "e") }
+ */
+static COSubtree *subtreeVariantB(ETUUID *aUUID)
+{
+	COSubtree *tree = subtreeInitialVersion(aUUID);
+	
+	[tree addObject: @"e"
+ toOrderedAttribute: @"letters"
+			atIndex: 3
+			   type: [COType arrayWithPrimitiveType: [COType stringType]]];
+	
+	return tree;
+}
+
+
+
+
+
+
+
+static void testPullComplex()
+{
+	COStore *store = setupStore();
+	
+	COPersistentRootEditingContext *ctx = [store rootContext];
+	COSubtree *iroot = [COSubtree subtree];
+	
+	[ctx setPersistentRootTree: iroot];
+	
+	ETUUID *contentsUUID = [ETUUID UUID];
+	
+	COSubtree *i1 = [[COSubtreeFactory factory] createPersistentRootWithRootItem: subtreeInitialVersion(contentsUUID)
+																	 displayName: @"My Document"
+																		   store: store];
+	
+	// set up a second branch, branch B.
+	
+	COSubtree *u1BranchA = [[COSubtreeFactory factory] currentBranchOfPersistentRoot: i1];
+	COSubtree *u1BranchB = [[COSubtreeFactory factory] createBranchOfPersistentRoot: i1];
+	[u1BranchA setPrimitiveValue: @"Branch A" forAttribute: @"name" type: [COType stringType]];
+	[u1BranchB setPrimitiveValue: @"Branch B" forAttribute: @"name" type: [COType stringType]];
+	
+	
+	[iroot addTree: i1];
+	
+	[ctx commitWithMetadata: nil];
+	
+	
+	// make a commit on branch A
+	
+	{
+		COPersistentRootEditingContext *ctx2 = [ctx editingContextForEditingEmbdeddedPersistentRoot: i1];
+		[ctx2 setPersistentRootTree: subtreeVariantA(contentsUUID)];
+		[ctx2 commitWithMetadata: nil];
+	}
+	
+	
+	// reopen the context to avoid a merge.
+	// FIXME: shouldn't be necessary
+	
+	ctx = [store rootContext];
+	i1 = [[ctx persistentRootTree] subtreeWithUUID: [i1 UUID]];
+	u1BranchA = [[ctx persistentRootTree] subtreeWithUUID: [u1BranchA UUID]];
+	u1BranchB = [[ctx persistentRootTree] subtreeWithUUID: [u1BranchB UUID]];
+	
+
+	// test that we can read the document contents as expected.
+	
+	EWTestEqual(A(@"a", @"b", @"c", @"d"),
+				[[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchA] persistentRootTree] valueForAttribute: @"letters"]);
+	EWTestEqual(A(@"b", @"c", @"d"),
+				[[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchB] persistentRootTree] valueForAttribute: @"letters"]);
+	
+	
+	
+	// switch to Branch B
+	
+	[[COSubtreeFactory factory] setCurrentBranch:u1BranchB forPersistentRoot:i1];
+	[ctx commitWithMetadata: nil];
+	
+
+	// make a commit on branch B
+	
+	{
+		COPersistentRootEditingContext *ctx2 = [ctx editingContextForEditingEmbdeddedPersistentRoot: i1];
+		[ctx2 setPersistentRootTree: subtreeVariantB(contentsUUID)];
+		[ctx2 commitWithMetadata: nil];
+	}
+	
+	
+	
+	
+	
+	// reopen the context to avoid a merge.
+	// FIXME: shouldn't be necessary
+	
+	ctx = [store rootContext];
+	i1 = [[ctx persistentRootTree] subtreeWithUUID: [i1 UUID]];
+	u1BranchA = [[ctx persistentRootTree] subtreeWithUUID: [u1BranchA UUID]];
+	u1BranchB = [[ctx persistentRootTree] subtreeWithUUID: [u1BranchB UUID]];
+	
+	
+	
+	
+	// test that we can read the document contents as expected.
+	
+	EWTestEqual(A(@"a", @"b", @"c", @"d"),
+				[[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchA] persistentRootTree] valueForAttribute: @"letters"]);
+	EWTestEqual(A(@"b", @"c", @"d", @"e"),
+				[[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchB] persistentRootTree] valueForAttribute: @"letters"]);
+	
+
+	
+	
+	
+	// now, suppose we want to pull the changes made in branch A into branch B.
+	// this will be a full merge.
+	
+	[[COSubtreeFactory factory] pullChangesFromBranch: u1BranchA
+											 toBranch: u1BranchB
+												store: store];
+	
+	[ctx commitWithMetadata: nil];
+	
+	
+	EWTestEqual(A(@"a", @"b", @"c", @"d", @"e"),
+				[[[ctx editingContextForEditingBranchOfPersistentRoot: u1BranchB] persistentRootTree] valueForAttribute: @"letters"]);
+}
+
+
+void testPull()
+{
+	testPullSimpleFastForward();
+	testPullComplex();
 }
