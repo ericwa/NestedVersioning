7b61a6a5a862a09fbde8cd15730a2136f8b9f4cf
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-11 16:15:48 -0700

    add TestDiffPrimitives

---
+++ Tests/TestDiffPrimitives.m
@@ -0,0 +1,14 @@
+//
+//  TestDiffPrimitives.m
+//  nestedversioning
+//
+//  Created by Eric Wasylishen on 12-02-11.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TestDiffPrimitives.h"
+
+
+@implementation TestDiffPrimitives
+
+@end
