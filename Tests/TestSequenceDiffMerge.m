19d87a2e3fbcec432900f84280db2bb2a3205f73
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-14 20:51:25 -0700

    work on diff test

--- Tests/TestSequenceDiffMerge.m
+++ Tests/TestSequenceDiffMerge.m
@@ -1,4 +1,5 @@
 #import "TestCommon.h"
+#import "COSequenceDiff.h"
 
 @interface TestSequenceDiffMerge : NSObject <UKTest>
 {
@@ -10,24 +11,34 @@
 
 - (void) testBasic
 {
-	NSArray *array2 = A(@"A", @"c", @"d", @"zoo", @"e");
-	NSArray *array1 = A(@"a", @"b", @"c", @"d", @"e", @"f");
+	NSArray *array2 = A(@"A", @"b", @"d", @"zoo", @"e");
+	NSArray *array1 = A(@"a", @"b", @"c", @"d", @"e");
 	NSArray *array3 = A(@"A", @"b", @"c", @"e", @"foo");
 	
 	/**
-	 * a->A, remove b, insert 'zoo' after d, remove f
+	 * modify a->A, remove c, insert 'zoo' after d, remove f
 	 */
 	COArrayDiff *diff12 = [[COArrayDiff alloc] initWithFirstArray: array1 secondArray: array2 sourceIdentifier: @"diff12"];
+
+	/**
+	 * modify a->A, remove d, insert 'foo' after e
+	 */
 	COArrayDiff *diff13 = [[COArrayDiff alloc] initWithFirstArray: array1 secondArray: array3 sourceIdentifier: @"diff13"];
 	
 	UKObjectsEqual(array2, [diff12 arrayWithDiffAppliedTo: array1]);
 	UKObjectsEqual(array3, [diff13 arrayWithDiffAppliedTo: array1]);
 	
 	COArrayDiff *merged = (COArrayDiff *)[diff12 sequenceDiffByMergingWithDiff: diff13];
-	NSLog(@"Merge result: %@", merged);
-	// FIXME: test the merge result
-	//NSLog(@"Expected: a->A, remove b, delete d, insert 'zoo' after d, insert foo after e");
-
+	UKFalse([merged hasConflicts]);
+	
+	// Expected: {a->A nonconflicting}, remove c, remove d,  delete d, insert 'zoo' after d, insert foo after e"
+	
+	COSequenceEdit *edit1 = [[merged operations] objectAtIndex: 0];
+	
+	UKFalse([edit1 hasConflicts]);
+	UKIntsEqual(2, [[edit1 allEdits] count]);
+	UKTrue(NSEqualRanges(NSMakeRange(0, 1), [[[[edit1 allEdits] allObjects] objectAtIndex: 0] range]));
+	UKTrue(NSEqualRanges(NSMakeRange(0, 1), [[[[edit1 allEdits] allObjects] objectAtIndex: 1] range]));
 }
 
 @end
