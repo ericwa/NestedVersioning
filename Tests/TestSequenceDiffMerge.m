d7ac2ff6fbe97b6b1adb00434e2d110c6849a4cd
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-14 22:46:14 -0700

    test more complex array diff conflict

--- Tests/TestSequenceDiffMerge.m
+++ Tests/TestSequenceDiffMerge.m
@@ -121,4 +121,58 @@
 	UKFalse([edit1diff12 isEqualIgnoringSourceIdentifier: edit1diff13]);
 }
 
+- (void) testLessSimpleConflict
+{
+	/*
+	 
+	 in this example, <delete 'b'-'d'> will conflict with two changes, {b->X, d->Z}
+	 
+	 */
+	
+	NSArray *array2 = A(@"a",                   @"e");
+	NSArray *array1 = A(@"a", @"b", @"c", @"d", @"e");
+	NSArray *array3 = A(@"a", @"X", @"c", @"Z", @"e");
+	
+	COArrayDiff *diff12 = [[COArrayDiff alloc] initWithFirstArray: array1 secondArray: array2 sourceIdentifier: @"diff12"];
+	COArrayDiff *diff13 = [[COArrayDiff alloc] initWithFirstArray: array1 secondArray: array3 sourceIdentifier: @"diff13"];
+	
+	COArrayDiff *merged = (COArrayDiff *)[diff12 sequenceDiffByMergingWithDiff: diff13];
+	UKTrue([merged hasConflicts]);
+	UKIntsEqual(1, [[merged operations] count]);
+	
+	// Examine the (single) change group
+	
+	COOverlappingSequenceEditGroup *edit1 = (COOverlappingSequenceEditGroup *)[[merged operations] objectAtIndex: 0];
+	
+	UKObjectKindOf(edit1, COOverlappingSequenceEditGroup);
+	UKTrue([edit1 hasConflicts]);
+	
+	// Examine the <delete 'b'-'d'> change from diff12
+	
+	NSArray *edit1diff12Array = [edit1 editsForSourceIdentifier: @"diff12"];
+	UKIntsEqual(1, [edit1diff12Array count]);
+	
+	COSequenceDeletion *edit1diff12 = [edit1diff12Array objectAtIndex: 0];
+	UKObjectKindOf(edit1diff12, COSequenceDeletion);
+	UKTrue(NSEqualRanges(NSMakeRange(1, 3), [edit1diff12 range]));
+	UKObjectsEqual(@"diff12", [edit1diff12 sourceIdentifier]);
+	
+	// Examine the {b->X, d->Z} change from diff13
+	
+	NSArray *edit1diff13Array = [edit1 editsForSourceIdentifier: @"diff13"];
+	UKIntsEqual(2, [edit1diff13Array count]);
+	
+	COSequenceModification *edit1diff13_1 = [edit1diff13Array objectAtIndex: 0];
+	UKObjectKindOf(edit1diff13_1, COSequenceModification);
+	UKObjectsEqual(A(@"X"), [edit1diff13_1 insertedObject]);
+	UKTrue(NSEqualRanges(NSMakeRange(1, 1), [edit1diff13_1 range]));
+	UKObjectsEqual(@"diff13", [edit1diff13_1 sourceIdentifier]);
+	
+	COSequenceModification *edit1diff13_2 = [edit1diff13Array objectAtIndex: 1];
+	UKObjectKindOf(edit1diff13_2, COSequenceModification);
+	UKObjectsEqual(A(@"Z"), [edit1diff13_2 insertedObject]);
+	UKTrue(NSEqualRanges(NSMakeRange(3, 1), [edit1diff13_2 range]));
+	UKObjectsEqual(@"diff13", [edit1diff13_2 sourceIdentifier]);
+}
+
 @end
