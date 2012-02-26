757d793b5445c6842f2dcaccf8dca025c1587d07
Eric Wasylishen <ewasylishen@gmail.com>
2012-02-26 00:01:09 -0700

    port over TestRelationshipIntegrity

--- Tests/TestSubtree.m
+++ Tests/TestSubtree.m
@@ -118,4 +118,153 @@
 	UKObjectsEqual(t2, t2a);
 }
 
+
+// From ObjectMerging - TestRelationshipIntegrity
+
+- (void)testBasicRelationshipIntegrity
+{
+	// Test one-to-many relationships
+	
+	COSubtree *o1 = [COSubtree subtree];
+	COSubtree *o2 = [COSubtree subtree];
+	COSubtree *o3 = [COSubtree subtree];
+	
+	[o1 addTree: o2];
+	[o2 addTree: o3];
+	
+	UKNil([o1 parent]);
+	UKObjectsEqual(S(o2), [o1 contents]);
+	UKObjectsSame(o1, [o2 parent]);
+	UKObjectsEqual(S(o3), [o2 contents]);
+	UKObjectsSame(o2, [o3 parent]);
+	UKObjectsEqual([NSSet set], [o3 contents]);
+
+	
+	// FIXME: Not supported yet.
+	
+	// Test many-to-many relationships
+	
+//	COObject *t1 = [ctx insertObjectWithEntityName: @"Anonymous.COCollection"]; // See COObject.m for metamodel definition
+//	COObject *t2 = [ctx insertObjectWithEntityName: @"Anonymous.COCollection"];
+//	COObject *t3 = [ctx insertObjectWithEntityName: @"Anonymous.COCollection"];
+//	
+//	[t1 addObject: o1 forProperty: @"contents"];
+//	[t2 addObject: o1 forProperty: @"contents"];
+//	
+//	UKObjectsEqual(S(t1, t2), [o1 valueForProperty: @"parentCollections"]);
+//	
+//	[o2 addObject: t2 forProperty: @"parentCollections"];
+//	[o2 addObject: t3 forProperty: @"parentCollections"];
+//	
+//	UKObjectsEqual(S(o1), [t1 valueForProperty: @"contents"]);
+//	UKObjectsEqual(S(o1, o2), [t2 valueForProperty: @"contents"]);
+//	UKObjectsEqual(S(o2), [t3 valueForProperty: @"contents"]);
+//	
+//	[ctx release];
+//	[store release];
+//	DELETE_STORE;
+}
+
+- (void)testRelationshipIntegrityForMove
+{
+	COSubtree *o1 = [COSubtree subtree];
+	COSubtree *o2 = [COSubtree subtree];
+	COSubtree *o3 = [COSubtree subtree];
+	
+	[o1 addTree: o2];
+
+	UKObjectsEqual(S(o2), [o1 contents]);
+	UKObjectsEqual([NSSet set], [o3 contents]);
+	
+	[o3 addTree: o2]; // should add o2 to o3's contents, and remove o2 from o1
+	
+	UKObjectsEqual([NSSet set], [o1 contents]);
+	UKObjectsEqual(S(o2), [o3 contents]);
+	
+	// Check that removing an object from a group nullifys that object's parent group pointer
+	
+	[o3 removeSubtree: o2];
+	UKNil([o2 parent]);
+}
+
+//- (void)testRelationshipIntegrityMarksDamage
+//{
+//	COStore *store = [[COStore alloc] initWithURL: STORE_URL];
+//	COEditingContext *ctx = [[COEditingContext alloc] initWithStore: store];
+//	
+//	COObject *o1 = [ctx insertObjectWithEntityName: @"Anonymous.COContainer"]; // See COObject.m for metamodel definition
+//	COObject *o2 = [ctx insertObjectWithEntityName: @"Anonymous.COContainer"];
+//	COObject *o3 = [ctx insertObjectWithEntityName: @"Anonymous.COContainer"];
+//	[ctx commit];
+//	
+//	UKFalse([ctx objectHasChanges: [o1 UUID]]);
+//	UKFalse([ctx objectHasChanges: [o2 UUID]]);
+//	UKFalse([ctx objectHasChanges: [o3 UUID]]);
+//	
+//	[o2 setValue: o1 forProperty: @"parentContainer"]; // should add o2 to o1's contents
+//	UKTrue([ctx objectHasChanges: [o1 UUID]]);
+//	UKTrue([ctx objectHasChanges: [o2 UUID]]);
+//	UKFalse([ctx objectHasChanges: [o3 UUID]]);
+//	
+//	[ctx commit];
+//	UKFalse([ctx objectHasChanges: [o1 UUID]]);
+//	UKFalse([ctx objectHasChanges: [o2 UUID]]);
+//	UKFalse([ctx objectHasChanges: [o3 UUID]]);
+//	
+//	[o2 setValue: o3 forProperty: @"parentContainer"]; // should add o2 to o3's contents, and remove o2 from o1
+//	UKTrue([ctx objectHasChanges: [o1 UUID]]);
+//	UKTrue([ctx objectHasChanges: [o2 UUID]]);
+//	UKTrue([ctx objectHasChanges: [o3 UUID]]);
+//	
+//	[ctx commit];
+//	
+//	[o3 removeObject: o2 forProperty: @"contents"]; // should make o2's parentContainer nil
+//	UKFalse([ctx objectHasChanges: [o1 UUID]]);
+//	UKTrue([ctx objectHasChanges: [o2 UUID]]);
+//	UKTrue([ctx objectHasChanges: [o3 UUID]]);	
+//	
+//	[ctx release];
+//	[store release];
+//	DELETE_STORE;
+//}
+
+- (void)testShoppingList
+{
+	COSubtree *workspace = [COSubtree subtree];
+	COSubtree *document1 = [COSubtree subtree];
+	COSubtree *group1 = [COSubtree subtree];
+	COSubtree *leaf1 = [COSubtree subtree];
+	COSubtree *leaf2 = [COSubtree subtree];
+	COSubtree *group2 = [COSubtree subtree];
+	COSubtree *leaf3 = [COSubtree subtree];
+	
+	COSubtree *document2 = [COSubtree subtree];
+	
+	[workspace addTree: document1];
+	[workspace addTree: document2];
+	[document1 addTree: group1];
+	[group1 addTree: leaf1];
+	[group1 addTree: leaf2];	
+	[document1 addTree: group2];	
+	[group2 addTree: leaf3];
+	
+	// Now make some changes
+	
+	[group2 addTree: leaf2];
+	[document2 addTree: group2];
+	
+	UKObjectsSame(workspace, [document1 parent]);
+	UKObjectsSame(workspace, [document2 parent]);	
+	UKObjectsSame(document1, [group1 parent]);	
+	UKObjectsSame(document2, [group2 parent]);	
+	UKObjectsSame(group1, [leaf1 parent]);	
+	UKObjectsSame(group2, [leaf2 parent]);	
+	UKObjectsSame(group2, [leaf3 parent]);	
+	UKObjectsEqual(S(document1, document2), [workspace contents]);
+	UKObjectsEqual(S(group1), [document1 contents]);
+	UKObjectsEqual(S(group2), [document2 contents]);
+	UKObjectsEqual(S(leaf1), [group1 contents]);
+	UKObjectsEqual(S(leaf2, leaf3), [group2 contents]);
+}
+
 @end
