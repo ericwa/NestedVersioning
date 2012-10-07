4d1453b9715a975c80abcde607dc2bec70779188
Eric Wasylishen <ewasylishen@gmail.com>
2012-10-07 13:26:02 -0400

    delete commented out code

--- Typewriter/EWTypewriterWindowController.h
+++ Typewriter/EWTypewriterWindowController.h
@@ -1,5 +1,4 @@
 #import <Cocoa/Cocoa.h>
-//#define EWDragType @"org.etoile.storebrowser.rows"
 
 #import "EWTextStorage.h"
 #import "EWTextView.h"
@@ -10,49 +9,8 @@
     
     EWTextStorage *textStorage_;
     
-    BOOL isLoading_;
-    
-//	COPath *path;
-//	COStore *store;
-//	COPersistentRootEditingContext *ctx;
-//	EWPersistentRootOutlineRow *outlineModel;
-//	IBOutlet NSOutlineView *outlineView;
-//	
-//	IBOutlet EWHistoryGraphView *historyView;
-//	
-//	IBOutlet NSButton *highlightInParentButton;
-//	IBOutlet NSButton *undoButton;
-//	IBOutlet NSButton *redoButton;
-//	IBOutlet NSSplitView *splitter;
-//	
-//	NSMutableDictionary *expansion;
+    BOOL isLoading_;    
 }
-
-//- (NSOutlineView *)outlineView;
-//
-//- (BOOL) isExpanded: (EWPersistentRootOutlineRow*)aRow;
-//- (void) setExpanded: (BOOL)flag
-//			 forRow: (EWPersistentRootOutlineRow *)aRow;
-//
-//- (id)initWithPath: (COPath*)aPath
-//			 store: (COStore*)aStore;
-//
-//- (IBAction) highlightInParent: (id)sender;
-//- (IBAction) undo: (id)sender;
-//- (IBAction) redo: (id)sender;
-//
-//- (void) orderFrontAndHighlightItem: (COUUID*)aUUID;
-//
-//- (COSubtree *)branchItem;
-//
-///**
-// * Temporary hack...
-// */
-//- (void) reloadBrowser;
-//
-//- (COUUID *) currentCommit;
-//
-//- (NSArray *)selectedRows;
 
 - (void) loadDocumentTree: (COSubtree *)aTree;
 
