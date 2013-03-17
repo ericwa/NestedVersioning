e25b816c1832ceeaa7ef5ee462cbbba874cf9bd4
Eric Wasylishen <ewasylishen@gmail.com>
2013-03-17 03:28:55 -0400

    Expand TestSQLiteStore so it can do an arbitrary number of commits

--- Tests/TestSQLiteStore.m
+++ Tests/TestSQLiteStore.m
@@ -11,8 +11,8 @@
 
 @implementation TestSQLiteStore
 
-static const int NUM_CHILDREN = 60;
-static const int NUM_COMMITS = 100;
+static const int NUM_CHILDREN = 100;
+static const int NUM_COMMITS = 1000;
 
 static COUUID *rootUUID;
 static COUUID *childUUIDs[NUM_CHILDREN];
@@ -54,24 +54,51 @@ static COUUID *childUUIDs[NUM_CHILDREN];
     return rootItem;
 }
 
-- (COItem *) childItem: (int)i withLabel: (NSString *)label
+- (COItem *) initialChildItem: (int)i
 {
     COMutableItem *child = [[[COMutableItem alloc] initWithUUID: childUUIDs[i]] autorelease];
-    [child setValue: label
+    [child setValue: [self labelForCommit: 0 child: i]
        forAttribute: @"name"
                type: [COType stringType]];
     return child;
 }
 
+// returns index of the item that was changed at the given commit index
+static int itemChangedAtCommit(int i)
+{
+    return ((1 + i) * 9871) % NUM_CHILDREN;
+}
+
+- (NSString *) labelForCommit: (int)commit // 0..(NUM_COMMITS - 1)
+                        child: (int)child
+{
+    for (int i=commit; i>=0; i--)
+    {
+        if (itemChangedAtCommit(i) == child)
+        {
+            return [NSString stringWithFormat: @"modified %d in commit %d", child, i];
+        }
+    }
+    return [NSString stringWithFormat: @"child %d never modified!", child];
+}
+
+#define VALIDATE 0
+
 - (void)testBasic
 {
+//    for (int i=0; i<NUM_CHILDREN; i++)
+//    {
+//        NSLog(@"label: %@", [self labelForCommit: NUM_COMMITS - 1
+//                                           child: i]);
+//    }
+    
     // Set up the initial items
     
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity: NUM_CHILDREN+1];
     [dict setObject: [self initialRootItem] forKey: rootUUID];
     for (int i=0; i<NUM_CHILDREN; i++)
     {
-        [dict setObject: [self childItem: i withLabel: [NSString stringWithFormat: @"child %d", i]]
+        [dict setObject: [self initialChildItem: i]
                  forKey: childUUIDs[i]];
     }
     
@@ -84,12 +111,16 @@ static COUUID *childUUIDs[NUM_CHILDREN];
     // Commit a change to each object
     
     CORevisionID *lastCommitId = [[proot currentBranchState] currentState];
-    for (int i=0; i<NUM_CHILDREN; i++)
-    {        
-        // Make a change to childUUIDs[i]
+    for (int commit=1; commit<NUM_COMMITS; commit++)
+    {
+        int i = itemChangedAtCommit(commit);
+        
+        NSString *label = [self labelForCommit: commit child: i];
+        
+//        NSLog(@"item %d changed in commit %d - seting label to %@", i, commit, label);
         
         COMutableItem *item = [dict objectForKey: childUUIDs[i]];
-        [item setValue: [NSString stringWithFormat: @"modified child %d", i]
+        [item setValue:label
           forAttribute: @"name"];
         
         lastCommitId = [store writeItemTree: [[[COItemTree alloc] initWithItemForUUID: dict rootItemUUID: rootUUID] autorelease]
@@ -101,27 +132,26 @@ static COUUID *childUUIDs[NUM_CHILDREN];
     // Now traverse them in reverse order and test that the items are as expected.
     // There are NUM_CHILDREN + 1 commits (the initial one made by creating the persistent roots)
     
-    for (int rev=NUM_CHILDREN; rev>=0; rev--)
+    for (int rev=NUM_COMMITS-1; rev>=0; rev--)
     {
         COItemTree *tree = [store itemTreeForRevisionID: lastCommitId];
 
         // Check the state
-        
+#if VALIDATE
         UKObjectsEqual(rootUUID, [tree rootItemUUID]);
         UKObjectsEqual([dict objectForKey: rootUUID],
                        [tree itemForUUID: rootUUID]);
+        
         for (int i=0; i<NUM_CHILDREN; i++)
         {
             // on rev=NUM_CHILDREN, child[NUM_CHILDREN - 1]'s name was changed
             
-            NSString *expectedLabel = (i < rev)
-                ? [NSString stringWithFormat: @"modified child %d", i]
-                : [NSString stringWithFormat: @"child %d", i];
+            NSString *expectedLabel = [self labelForCommit: rev child: i];
             
             UKObjectsEqual(expectedLabel,
                            [[tree itemForUUID: childUUIDs[i]] valueForAttribute: @"name"]);
         }
-
+#endif
         // Step back one revision
         
         lastCommitId = [[store revisionForID: lastCommitId] parentRevisionID];
