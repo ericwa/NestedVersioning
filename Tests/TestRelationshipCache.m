677de08c718c392d0b4e60cef43f4c50ac65f59e
Eric Wasylishen <ewasylishen@gmail.com>
2013-03-28 01:49:40 -0400

    Expand test for CORelationshipCache

--- Tests/TestRelationshipCache.m
+++ Tests/TestRelationshipCache.m
@@ -32,26 +32,26 @@
                                        oldType: nil
                                       newValue: u2
                                        newType: [COType embeddedItemType]
-                                   forProperty: @"children"
+                                   forProperty: @"child"
                                       ofObject: u1];
     
     
-    UKObjectsEqual([CORelationshipRecord recordWithUUID: u1 property: @"children"], [cache parentForUUID: u2]);
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: u1 property: @"child"], [cache parentForUUID: u2]);
 
     [cache updateRelationshipCacheWithOldValue: nil
                                        oldType: nil
                                       newValue: u2
                                        newType: [COType embeddedItemType]
-                                   forProperty: @"children"
+                                   forProperty: @"child"
                                       ofObject: u3];
     
-    UKObjectsEqual([CORelationshipRecord recordWithUUID: u3 property: @"children"], [cache parentForUUID: u2]);
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: u3 property: @"child"], [cache parentForUUID: u2]);
     
     [cache updateRelationshipCacheWithOldValue: u2
                                        oldType: [COType embeddedItemType]
                                       newValue: nil
                                        newType: [COType embeddedItemType]
-                                   forProperty: @"children"
+                                   forProperty: @"child"
                                       ofObject: u3];
     
     UKNil([cache parentForUUID: u2]);
@@ -59,7 +59,47 @@
 
 - (void) testParentWithEmbeddedItemSet
 {
+    COUUID *p1 = [COUUID UUID];
+    COUUID *p2 = [COUUID UUID];
     
+    COUUID *u2 = [COUUID UUID];
+    COUUID *u3 = [COUUID UUID];
+    COUUID *u4 = [COUUID UUID];
+    
+    [cache updateRelationshipCacheWithOldValue: nil
+                                       oldType: nil
+                                      newValue: S(u2, u3)
+                                       newType: [[COType embeddedItemType] setType]
+                                   forProperty: @"children"
+                                      ofObject: p1];
+    
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p1 property: @"children"], [cache parentForUUID: u2]);
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p1 property: @"children"], [cache parentForUUID: u3]);
+    UKNil([cache parentForUUID: u4]);
+    
+    [cache updateRelationshipCacheWithOldValue: S(u2, u3)
+                                       oldType: [[COType embeddedItemType] setType]
+                                      newValue: S(u2, u4)
+                                       newType: [[COType embeddedItemType] setType]
+                                   forProperty: @"children"
+                                      ofObject: p1];
+    
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p1 property: @"children"], [cache parentForUUID: u2]);
+    UKNil([cache parentForUUID: u3]);
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p1 property: @"children"], [cache parentForUUID: u4]);
+
+    // Test that adding u2 to p2 updates the parent correctly
+    
+    [cache updateRelationshipCacheWithOldValue: nil
+                                       oldType: nil
+                                      newValue: S(u2)
+                                       newType: [[COType embeddedItemType] setType]
+                                   forProperty: @"children"
+                                      ofObject: p2];
+
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p2 property: @"children"], [cache parentForUUID: u2]);
+    UKNil([cache parentForUUID: u3]);
+    UKObjectsEqual([CORelationshipRecord recordWithUUID: p1 property: @"children"], [cache parentForUUID: u4]);    
 }
 
 - (void) testReferences
@@ -102,6 +142,56 @@
 
     UKObjectsEqual(S([CORelationshipRecord recordWithUUID: u1 property: @"link1"],
                      [CORelationshipRecord recordWithUUID: u1 property: @"link2"]), [cache referrersForUUID: u2]);
+}
+
+- (void) testReferencesSet
+{
+    COUUID *g1 = [COUUID UUID];
+    COUUID *g2 = [COUUID UUID];
+    
+    COUUID *u1 = [COUUID UUID];
+    COUUID *u2 = [COUUID UUID];
+    COUUID *u3 = [COUUID UUID];
+    
+    [cache updateRelationshipCacheWithOldValue: nil
+                                       oldType: nil
+                                      newValue: S(u1, u2)
+                                       newType: [[COType referenceType] setType]
+                                   forProperty: @"groupContents"
+                                      ofObject: g1];
+    
+    [cache updateRelationshipCacheWithOldValue: nil
+                                       oldType: nil
+                                      newValue: S(u2, u3)
+                                       newType: [[COType referenceType] setType]
+                                   forProperty: @"groupContents"
+                                      ofObject: g2];
+    
+    UKObjectsEqual(S([CORelationshipRecord recordWithUUID: g1 property: @"groupContents"]), [cache referrersForUUID: u1]);
+
+    UKObjectsEqual(S([CORelationshipRecord recordWithUUID: g1 property: @"groupContents"],
+                     [CORelationshipRecord recordWithUUID: g2 property: @"groupContents"]), [cache referrersForUUID: u2]);
+
+    UKObjectsEqual(S([CORelationshipRecord recordWithUUID: g2 property: @"groupContents"]), [cache referrersForUUID: u3]);
+
+    [cache updateRelationshipCacheWithOldValue: S(u1, u2)
+                                       oldType: [[COType referenceType] setType]
+                                      newValue: S(u1, u3)
+                                       newType: [[COType referenceType] setType]
+                                   forProperty: @"groupContents"
+                                      ofObject: g1];
+    
+    [cache updateRelationshipCacheWithOldValue: S(u2, u3)
+                                       oldType: [[COType referenceType] setType]
+                                      newValue: [NSSet set]
+                                       newType: [[COType referenceType] setType]
+                                   forProperty: @"groupContents"
+                                      ofObject: g2];
+    
+    
+    UKObjectsEqual(S([CORelationshipRecord recordWithUUID: g1 property: @"groupContents"]), [cache referrersForUUID: u1]);    
+    UKObjectsEqual([NSSet set], [cache referrersForUUID: u2]);
+    UKObjectsEqual(S([CORelationshipRecord recordWithUUID: g1 property: @"groupContents"]), [cache referrersForUUID: u3]);
 }
 
 @end
