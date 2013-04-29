e12cd489225eed0defd8b4fe1b1910421944fbe8
Eric Wasylishen <ewasylishen@gmail.com>
2013-04-29 03:30:40 -0400

    Start a multi-persistent root test case

---
+++ Tests/TestSQLiteStoreMultiPersistentRoots.m
@@ -0,0 +1,66 @@
+#import "TestCommon.h"
+
+@interface TestSQLiteStoreMultiPersistentRoots : COSQLiteStoreTestCase <UKTest>
+{
+    COPersistentRootState *docProot;
+    COPersistentRootState *tagProot;
+}
+@end
+
+@implementation TestSQLiteStoreMultiPersistentRoots
+
+static COUUID *docUUID;
+static COUUID *tagUUID;
+
++ (void) initialize
+{
+    if (self == [TestSQLiteStoreMultiPersistentRoots class])
+    {
+        docUUID = [[COUUID alloc] init];
+        tagUUID = [[COUUID alloc] init];
+    }
+}
+
+- (COItemTree *) tagItemTreeWithDocProoUUID: (COUUID*)aUUID
+{
+    COMutableItem *rootItem = [[[COMutableItem alloc] initWithUUID: tagUUID] autorelease];
+    [rootItem setValue: @"favourites" forAttribute: @"name" type: kCOStringType];
+    [rootItem setValue: S([COPath pathWithPersistentRoot: aUUID])
+          forAttribute: @"taggedDocuments"
+                  type: kCOPathType | kCOSetType];
+
+    return [COItemTree treeWithItemsRootFirst: A(rootItem)];
+}
+
+- (COItemTree *) docItemTree
+{
+    COMutableItem *rootItem = [[[COMutableItem alloc] initWithUUID: tagUUID] autorelease];
+    [rootItem setValue: @"my document" forAttribute: @"name" type: kCOStringType];
+    
+    return [COItemTree treeWithItemsRootFirst: A(rootItem)];
+}
+
+- (id) init
+{
+    SUPERINIT;
+    
+    ASSIGN(docProot, [store createPersistentRootWithInitialContents: [self docItemTree]
+                                                         metadata: nil]);
+    ASSIGN(tagProot, [store createPersistentRootWithInitialContents: [self tagItemTreeWithDocProoUUID: [docProot UUID]]
+                                                         metadata: nil]);
+    return self;
+}
+
+- (void) dealloc
+{
+    [tagProot release];
+    [docProot release];
+    [super dealloc];
+}
+
+- (void) testSearch
+{
+    UKFail();
+}
+
+@end
